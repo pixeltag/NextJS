c1ea6b9d7649b5514196983130c946004ff62b61
ihab Youssef <pixeltagnet@gmail.com>
2019-06-22 18:07:10 +0200

    init evergreen Next with Sass

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,9 +1,13 @@
 import Page from '../layouts/page';
+import { Button } from 'evergreen-ui';
+import '../assets/scss/main.scss'
 
 const Index = (props) => {
     return (
         <Page>
             <h1>Hello from NextJS</h1>
+            <Button>I am using 🌲 Evergreen!</Button>
+
         </Page>
     )
 }
