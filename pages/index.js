641d65a0b9d2236cf383dff39f3180fe3df9d348
ihab Youssef <pixeltagnet@gmail.com>
2019-06-25 17:21:04 +0200

    Handling_Errors_with_Built-in_Error_Page_and_Try__Catch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+
 class Index extends React.Component {
+
+    static async getInitialProps() {
+        let stories;
+        try {
+            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            stories = await response.json();
+        } catch (error) {
+            stories= [];
+        }
+        return { stories };
+    }
+
     render() {
-        return (
-            <div>Hello Hacker News</div>
+        
+        const { stories } = this.props; 
+        
+        if(stories.length === 0) {
+                return <Error statusCode={503} />
+        }
+            
+            return (
+            <div>
+                <h2>Hackers News</h2>
+                <div className="hackers-news-wrapper">
+                    { stories.map(story => {
+                       return <h4 key={story.id}>{story.title}</h4>
+                    })}
+                </div>
+            </div>
         )
     }
 }
