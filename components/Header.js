159cb380b9ad3fb43cdbc6515b0fde9006a97bc6
ihab Youssef <pixeltagnet@gmail.com>
2019-06-25 22:07:21 +0200

    Building_the_Individual_Story_Page

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+
 const Header = () => (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
         <div className="container">
             <div className="row">
-                <h5 className="my-0 mr-md-auto font-weight-normal">Hackers News</h5>
+
+                    <h5 className="my-0 mr-md-auto font-weight-normal">
+                        <Link href="./">
+                          <a>Hackers News</a>
+                        </Link>
+                    </h5>
+
                 <nav className="my-2 my-md-0 mr-md-3">
                     <a className="p-2 text-dark" href="#">Features</a>
                     <a className="p-2 text-dark" href="#">Enterprise</a>
---
+++ pages/story.js
@@ -0,0 +1,45 @@
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+import Layout from '../components/Layout';
+
+class Story extends React.Component {
+
+    static async getInitialProps({req , res , query})  {
+        let storyId = Number(query.id);
+        let story;
+        try {
+            const response = await fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`)
+            story = await response.json();
+        } catch (error) {
+            console.log(error);
+            story = null;
+        }
+        return { story };
+
+    }
+
+    render() {
+        const {story} = this.props;
+        if(story.length === 0) {
+            return ( <Error statusCode="503" />)
+        }
+        return (
+            <Layout title={story.title}>
+                    <section className="jumbotron text-center">
+                        <div className="container">
+                        <h1 className="jumbotron-heading">{story.title}</h1>
+                        <span className="lead text-muted mr-4">{story.comments_count || 0} Comments</span>
+                        <span className="lead text-muted mr-4">{story.points || 0} Points</span>
+                        <span className="lead text-muted mr-4">{story.time_ago || 0}</span>
+                        <p>
+                            <a href="#" className="btn btn-primary my-2">Main call to action</a>
+                            <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+                        </p>
+                        </div>
+                    </section>
+            </Layout>
+        )
+    }
+}
+
+export default Story;
