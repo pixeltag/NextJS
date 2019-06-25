751f6b277f146803e0abbb0338a0399772e28969
ihab Youssef <pixeltagnet@gmail.com>
2019-06-25 21:41:55 +0200

    Fetching_Next_Page_of_Stories,_Adding_Pagination_with_Query_Params

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,25 @@ import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
 import StoriesList from '../components/StoriesList';
 import Layout from '../components/Layout';
+import Link from 'next/link';
 
 class Index extends React.Component {
 
-    static async getInitialProps() {
+    static async getInitialProps({ req , res , query}) {
         let stories;
+        let page = Number(query.page) || 1;
         try {
-            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
             stories = await response.json();
         } catch (error) {
             stories= [];
         }
-        return { stories };
+        return { stories  , page};
     }
 
     render() {
 
-        const { stories } = this.props;
+        const { stories , page  } = this.props;
 
         if(stories.length === 0) {
                 return <Error statusCode={503} />
@@ -28,8 +30,22 @@ class Index extends React.Component {
             <Layout title="Hacker News" description="Hacker News Blog based on Next Js Frame work">
                 <div className="hackers-news-wrapper">
                     <StoriesList  stories={stories}  />
+                    <div className="pager">
+                        <Link href={`/?page=${page - 1}`}>
+                            <button className="btn btn-primary">
+                                    Prev Page {page - 1}
+                            </button>
+                        </Link>
+                        <Link href={`/?page=${page + 1}`}>
+                            <button className="btn btn-primary">
+                                    Next Page {page + 1}
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </Layout>
+
+
         )
     }
 }
